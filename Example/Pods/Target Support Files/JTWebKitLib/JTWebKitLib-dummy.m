6c5b2f43336aa540f4f9621b2648273976860314
lushitong <lushitong@qq.com>
2020-03-11 16:35:22 +0800

    初始化项目
    Signed-off-by: lushitong <lushitong@qq.com>

---
+++ Example/Pods/Target Support Files/JTWebKitLib/JTWebKitLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JTWebKitLib : NSObject
+@end
+@implementation PodsDummy_JTWebKitLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-JTWebKitLib_Tests/Pods-JTWebKitLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JTWebKitLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JTWebKitLib_Tests
+@end
